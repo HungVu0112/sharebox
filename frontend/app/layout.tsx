f54d468d25da5c5dfc9efd1a5504adf3dffce66c
HungVu0112 <pcsolaggy123@gmail.com>
2024-09-28 09:41:40 +0700

    Adding Web Icon & Web Title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/parachute-box-solid.svg" />
+      </head>
       <body>
         {children}
       </body>
