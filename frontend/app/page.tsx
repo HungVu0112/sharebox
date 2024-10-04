e60239134cc5226e79a682eee9b46603a79ddb23
HungVu0112 <pcsolaggy123@gmail.com>
2024-10-04 11:38:42 +0700

    Home Page Test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,8 +8,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className="w-full h-[100vh] bg-red-400">
+      
     </div>
   );
 }
