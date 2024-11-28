3c3fb69e21257c1cbbef66500e9a01bc8c68a89a
Noblesse1607 <luongduc0912511093@gmail.com>
2024-11-28 22:01:11 +0700

    Add: UserControllerTest

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import loginImage from '../public/login_image.svg';
+import loginImage2 from '../public/login_image_2.svg';
+import gmailIcon from '../public/envelope-regular.svg';
+import lockIcon from '../public/lock-solid.svg';
+import googleIcon from '../public/google_icon.svg';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full h-[100vh] text-center">
-        <p className="text-mainColor">HELLO</p>
+    <div>
+      Home Page
     </div>
   );
 }
